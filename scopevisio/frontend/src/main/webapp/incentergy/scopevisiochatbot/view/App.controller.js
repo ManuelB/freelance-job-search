b0914a4ad9d6ee82a281967ddc477d230f5feaaa
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2020-10-29 21:29:19 +0100

    Corrected search string for is null

--- scopevisio/frontend/src/main/webapp/incentergy/scopevisiochatbot/view/App.controller.js
+++ scopevisio/frontend/src/main/webapp/incentergy/scopevisiochatbot/view/App.controller.js
@@ -35,7 +35,7 @@ sap.ui.define([
 					      {
 					        "field":"customBoolean1",
 						 	"value": true,
-						 	"operator": "notequal"
+						 	"operator": "is null, notequal"
 					      }
 					   ]
 					}),
